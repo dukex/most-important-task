0e79169952ac2c65214be8dd8f5f266c6465a5d1
Emerson Almeida <emersonalmeidax@gmail.com>
2019-01-15 20:56:12 -0200

    get task fallback

--- extension.js
+++ extension.js
@@ -1,66 +1,113 @@
 const St = imports.gi.St;
 const Main = imports.ui.main;
-const Lang = imports.lang;
 const Soup = imports.gi.Soup;
 const Mainloop = imports.mainloop;
 const Goa = imports.gi.Goa;
 
-const Account = new Lang.Class({
-  Name: 'Account',
-  _init: function() {
-
-  },
-  get_token: function () {
+class Account {
+  get_token() {
     const client = Goa.Client.new_sync(null);
+
+    log("client:");
+    log(client);
+
     const access_token = client.get_accounts()
-      .filter(a => a.get_account().provider_type === 'todoist')
-      .map(function(account) {
-        return account.get_oauth2_based().call_get_access_token_sync(null);
+      .map(a => {
+        log("provider_type:");
+        log(a.get_account().provider_type);
+        return a;
       })
-      .map(function(result) {
-        let [, access_token, ] = result;
-        return access_token;
-      });
+      .filter(a => a.get_account().provider_type === 'todoist')
+      .map(account => account.get_oauth2_based().call_get_access_token_sync(null))
+      .map(([, access_token, ]) => access_token);
 
     return access_token[0];
   }
-});
+}
 
 let button, label, account, _timeout, _httpSession;
 
-function request(method, path, params, fn) {
-  let message = Soup.form_request_new_from_hash(method, `https://beta.todoist.com/API/v8/${path}`, params);
+const request = (method, path, params) => {
+  const message = Soup.form_request_new_from_hash(method, `https://beta.todoist.com/API/v8/${path}`, params);
   message.request_headers.append("Authorization", `Bearer ${account.get_token()}`);
 
-  _httpSession.queue_message(message, Lang.bind(this,
-    function (_httpSession, message) {
-      if (message.status_code !== 200)
-        return;
-      let json = JSON.parse(message.response_body.data);
+  return new Promise((resolve, reject) => {
+    _httpSession.queue_message(message, (_httpSession, message) => {
+        if (message.status_code !== 200) {
+          return reject();
+        }
 
-      fn(json);
+        let json = JSON.parse(message.response_body.data);
+
+        resolve(json);
+      }
+    );
+  });
+}
+
+const fetchTask = (query) => {
+  return new Promise((resolve, reject) => {
+    request("GET", "tasks", { filter: query })
+    .then((tasks) => {
+      const task = tasks.sort((a,b) => new Date(a.due.date) - new Date(b.due.date))[0];
+      if(task) {
+        resolve(task);
+      } else {
+        reject();
+      }
+    }, reject);
+  });
+}
+
+const asyncFetchTask = (query) => {
+  return (e) => {
+    log("query:");
+    log(query);
+
+    log("error:");
+    log(e);
+    return fetchTask(query);
+  }
+}
+
+  var setText = task => {
+    if(task) {
+      label.set_text(task.content);
+      return nil;
+    } else {
+      throw false;
     }
-  ));
+  }
+
+
+function assignTask() {
+  fetchTask("@mit & (today | overdue)")
+  .then(setText, () => {
+    fetchTask("#READ")
+    .then(setText, () => {
+      fetchTask("7 days")
+      .then(setText, () => {
+        fetchTask("assigned to: me")
+        .then(setText, () => {
+          setText("=)");
+        });
+      });
+    });
+  });
 }
 
 function refresh() {
+  log("refresh");
+
   if (_timeout) {
     Mainloop.source_remove(_timeout);
     _timeout = null;
   }
 
-  request("GET", "tasks", { filter: "@mit & (today | overdue)" }, function(tasks) {
-    task = tasks.sort((a,b) => new Date(a.due.date) - new Date(b.due.date))[0];
-    if(task) {
-      label.set_text(task.content);
-    } else {
-      label.set_text("Definir M.I.T.");
-    }
-  });
+  assignTask()
 
-  _timeout = Mainloop.timeout_add_seconds(60, Lang.bind(this, refresh));
+  _timeout = Mainloop.timeout_add_seconds(60, refresh);
 }
-
 
 function init() {
   account = new Account();
@@ -88,6 +135,7 @@ function init() {
 
 function enable() {
   Main.panel._centerBox.insert_child_at_index(button, 0);
+  log("enable");
   refresh();
 }
 
