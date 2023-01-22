d03edb4c1baefb2a34dadee838b85962171d85f1
Emerson Almeida <emersonalmeidax@gmail.com>
2023-01-21 21:47:13 -0300

    update to new gnome and start to work with rules

--- extension.js
+++ extension.js
@@ -6,145 +6,215 @@ const GLib = imports.gi.GLib;
 
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 
-let settings, button, label, account, _timeout, _httpSession, gschema, currentTask;
+let settings,
+  button,
+  label,
+  account,
+  _timeout,
+  _httpSession,
+  gschema,
+  cancellable,
+  currentTask;
 
 class Account {
-    getToken() {
-        return settings.get_string('todoist-api-token');
-    }
+  getToken() {
+    return settings.get_string("todoist-api-token");
+  }
+
+  getSearch() {
+    return settings.get_string("todoist-search");
+  }
+
+  getRules() {
+    return settings.get_strv('todoist-search-rules')
+  }
 }
 
+const logTodoist = (message) => log("[mit@emersonalmeidax.wtf]["+ new Date() +"] " + message);
+
 const request = (method, path, params) => {
-    const message = Soup.form_request_new_from_hash(method, `https://api.todoist.com/rest/v1/${path}`, params);
-    message.request_headers.append('Authorization', `Bearer ${account.getToken()}`);
+  return new Promise((resolve, reject) => {
+    try {
 
-    return new Promise((resolve, reject) => {
-        _httpSession.queue_message(message, (_session, response) => {
-            if (response.status_code !== 200)
-                return reject(response);
+    logTodoist("todoist: request(" + method + ", " + path + ", " + params);
 
-            let json = JSON.parse(response.response_body.data);
+    const query = Soup.form_encode_hash(params); //Object.keys(params).map((key) => `${key}=${params[key]}`).join("&")
 
-            return resolve(json);
-        });
-    });
+    logTodoist(
+      "curl -X" +
+        method +
+        " -H 'Authorization: " +
+        `Bearer ${account.getToken()}' ` +
+        ` 'https://api.todoist.com/rest/v2/${path}?${query}'`
+    );
+
+    const message = Soup.Message.new_from_encoded_form(
+      method,
+      `https://api.todoist.com/rest/v2/${path}`,
+      query
+    );
+
+    logTodoist("message" + message);
+
+    message.request_headers.append(
+      "Authorization",
+      `Bearer ${account.getToken()}`
+    );
+
+    const bytes = _httpSession.send_and_read(message, cancellable);
+
+    decoder = new TextDecoder()
+
+    logTodoist("bytesL: " + bytes)
+    json = JSON.parse(decoder.decode(bytes.get_data()));
+    logTodoist("json"  + json);
+    resolve(json)
+
+  } catch (error) {
+    logTodoist("error" + error)
+    reject(error)
+  }
+  });
 };
 
-const fetchTask = query => {
-    return new Promise((resolve, reject) => {
-        request('GET', 'tasks', { filter: query })
-        .then(tasks => {
-            const task = tasks.sort((a, b) => new Date(a.due.date) - new Date(b.due.date))[0];
-            if (task)
-                resolve(task);
-            else
-                reject(task);
-        }, reject);
-    });
+const fetchTask = (query) => {
+  logTodoist("todoist: fetchTask()");
+
+
+
+  return new Promise((resolve, reject) => {
+    request("GET", "tasks", { filter: query }).then((tasks) => {
+      const task = tasks.sort(
+        (a, b) => new Date(a.due.date) - new Date(b.due.date)
+      )[0];
+      if (task) resolve(task);
+      else reject(task);
+    }, reject);
+  });
 };
 
-
-var setTask = task => {
-    if (task) {
-        if (task.url) {
-            currentTask = task;
-            log(JSON.stringify(currentTask));
-        }
-
-        label.set_text(task.content);
-        return task;
-    } else {
-        throw task;
+var setTask = (task) => {
+  if (task) {
+    if (task.url) {
+      currentTask = task;
+      logTodoist(JSON.stringify(currentTask));
     }
+
+    label.set_text(task.content);
+    return task;
+  } else {
+    throw task;
+  }
 };
 
-var logRejectionError = fn => error => {
-    log(`rejected: ${JSON.stringify(error)}`);
-    fn(error);
+var logTodoistRejectionError = (fn) => (error) => {
+  logTodoist(`rejected: ${JSON.stringify(error)}`);
+  fn(error);
 };
 
 function assignTask() {
-    fetchTask('@mit & (today | overdue)')
-    .then(setTask, logRejectionError(() => {
-        return fetchTask('7 days');
-    }))
-    .then(setTask, logRejectionError(() => {
-        return fetchTask('assigned to: me');
-    }))
-    .then(setTask, logRejectionError(() => {
-        setTask({ content: '=)' });
-    }));
+
+
+  logTodoist(typeof account.getRules());
+
+  fetchTask(account.getSearch())
+    .then(
+      setTask,
+      logTodoistRejectionError(() => {
+        return fetchTask("7 days");
+      })
+    )
+    .then(
+      setTask,
+      logTodoistRejectionError(() => {
+        return fetchTask("assigned to: me");
+      })
+    )
+    .then(
+      setTask,
+      logTodoistRejectionError(() => {
+        setTask({ content: "=)" });
+      })
+    );
 }
 
 function refresh() {
-    log('refresh');
+  logTodoist("refresh");
 
-    if (_timeout) {
-        GLib.source_remove(_timeout);
-        _timeout = null;
-    }
+  if (_timeout) {
+    GLib.source_remove(_timeout);
+    _timeout = null;
+  }
 
-    assignTask();
+  assignTask();
 
-    // TODO: move the seconds to settings
-    _timeout = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 60, refresh);
+  // TODO: move the seconds to settings
+  _timeout = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 60, refresh);
 }
 
 function init() {
+  logTodoist("todoist: init()");
 }
 
 function enable() {
-    gschema = Gio.SettingsSchemaSource.new_from_directory(
-        Me.dir.get_child('schemas').get_path(),
-        Gio.SettingsSchemaSource.get_default(),
-        false,
-    );
+  logTodoist("todoist: enable()");
 
-    settings = new Gio.Settings({
-        settings_schema: gschema.lookup('org.gnome.shell.extensions.mit', true),
-    });
+  gschema = Gio.SettingsSchemaSource.new_from_directory(
+    Me.dir.get_child("schemas").get_path(),
+    Gio.SettingsSchemaSource.get_default(),
+    false
+  );
 
-    account = new Account();
+  settings = new Gio.Settings({
+    settings_schema: gschema.lookup("org.gnome.shell.extensions.mit", true),
+  });
 
-    _httpSession = new Soup.Session();
+  account = new Account();
+  cancellable = new Gio.Cancellable()
+  _httpSession = new Soup.Session();
 
-    button = new St.Bin({ style_class: 'panel-button',
-        reactive: true,
-        can_focus: true,
-        x_fill: true,
-        y_fill: false,
-        track_hover: true });
+  button = new St.Button({
+    style_class: "panel-button",
+    reactive: true,
+    can_focus: true,
+    // x_fill: true,
+    // y_fill: false,
+    track_hover: true,
+  });
 
-    label = new St.Label({ text: 'Loading...' });
-    button.set_child(label);
+  label = new St.Label({ text: "Loading..." });
+  button.set_child(label);
 
-    button.connect('button-press-event', () => {
-        if (currentTask) {
-            log(JSON.stringify(currentTask));
-            let file = imports.gi.Gio.File.new_for_uri(currentTask.url);
-            file.query_exists(null);
-            file.query_default_handler(null).launch([file], null);
-        }
-    });
+  button.connect("button-press-event", () => {
+    if (currentTask) {
+      logTodoist(JSON.stringify(currentTask));
+      let file = imports.gi.Gio.File.new_for_uri(currentTask.url);
+      file.query_exists(null);
+      file.query_default_handler(null).launch([file], null);
+    }
+  });
 
-    Main.panel._centerBox.insert_child_at_index(button, 0);
-    log('enable');
-    refresh();
+  Main.panel._centerBox.insert_child_at_index(button, 0);
+  logTodoist("enable");
+  refresh();
 }
 
 function disable() {
-    _httpSession.abort();
-    Main.panel._centerBox.remove_child(button);
-    if (_timeout) {
-        GLib.source_remove(_timeout);
-        _timeout = null;
-    }
+  _httpSession.abort();
+  // Main.panel._centerBox.remove_child(button);
+  if (_timeout) {
+    GLib.source_remove(_timeout);
+    _timeout = null;
+  }
 
-    _httpSession = null;
-    account = null;
-    button = null;
-    currentTask = null;
-    gschema = null;
-    label = null;
-    settings = null;
+  cancellable.cancel();
+
+  cancellable = null
+  _httpSession = null;
+  account = null;
+  button = null;
+  currentTask = null;
+  gschema = null;
+  label = null;
+  settings = null;
 }
--- prefs.js
+++ prefs.js
@@ -1,54 +1,340 @@
-'use strict';
+"use strict";
 
-const Gtk = imports.gi.Gtk;
-const Gio = imports.gi.Gio;
+const {Adw, Gio, GLib, GObject, Gtk, Pango} = imports.gi;
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 
 let settings;
 
-function init() {
+class NewItem extends GObject.Object {}
+GObject.registerClass(NewItem);
+
+class NewItemModel extends GObject.Object {
+  static [GObject.interfaces] = [Gio.ListModel];
+  static {
+      GObject.registerClass(this);
+  }
+
+  #item = new NewItem();
+
+  vfunc_get_item_type() {
+      return NewItem;
+  }
+
+  vfunc_get_n_items() {
+      return 1;
+  }
+
+  vfunc_get_item(_pos) {
+      return this.#item;
+  }
 }
 
-function buildPrefsWidget() {
-    let gschema = Gio.SettingsSchemaSource.new_from_directory(
-        Me.dir.get_child('schemas').get_path(),
-        Gio.SettingsSchemaSource.get_default(),
-        false,
-    );
 
-    settings = new Gio.Settings({
-        settings_schema: gschema.lookup('org.gnome.shell.extensions.mit', true),
-    });
 
-    const prefsWidget = new Gtk.Grid({
-        margin: 18,
-        column_spacing: 12,
-        row_spacing: 12,
-        visible: true,
-    });
+let gschema = Gio.SettingsSchemaSource.new_from_directory(
+  Me.dir.get_child("schemas").get_path(),
+  Gio.SettingsSchemaSource.get_default(),
+  false
+);
 
-    const title = new Gtk.Label({
-        label: `<b>${Me.metadata.name} Preferences</b>`,
-        halign: Gtk.Align.START,
-        use_markup: true,
-        visible: true,
-    });
-    prefsWidget.attach(title, 0, 0, 2, 1);
 
-    const todoistTokenLabel = new Gtk.Label({
-        label: 'Todoist API Token:',
-        halign: Gtk.Align.START,
-        visible: true,
-    });
-    prefsWidget.attach(todoistTokenLabel, 0, 1, 1, 1);
+class RulesList extends GObject.Object {
+  static [GObject.interfaces] = [Gio.ListModel];
+  static {
+      GObject.registerClass(this);
+  }
 
-    const todoistTokenEntry = new Gtk.Entry({
-        visible: true,
-        text: settings.get_string('todoist-api-token'),
-    });
-    prefsWidget.attach(todoistTokenEntry, 1, 1, 1, 1);
+  #settings =  new Gio.Settings({
+    settings_schema: gschema.lookup("org.gnome.shell.extensions.mit", true),
+  });
+  #names = this.#settings.get_strv("todoist-search-rules");
+  #items = Gtk.StringList.new(this.#names);
+  #changedId;
 
-    settings.bind('todoist-api-token', todoistTokenEntry, 'text', Gio.SettingsBindFlags.DEFAULT);
+  constructor() {
+      super();
 
-    return prefsWidget;
+      this.#changedId =
+          this.#settings.connect(`changed::${"todoist-search-rules"}`, () => {
+              const removed = this.#names.length;
+              this.#names = this.#settings.get_strv("todoist-search-rules");
+              this.#items.splice(0, removed, this.#names);
+              this.items_changed(0, removed, this.#names.length);
+          });
+  }
+
+  append() {
+      const name = 'Rule %d'.format(this.#names.length + 1);
+
+      this.#names.push(name);
+      this.#settings.block_signal_handler(this.#changedId);
+      this.#settings.set_strv("todoist-search-rules", this.#names);
+      this.#settings.unblock_signal_handler(this.#changedId);
+
+      const pos = this.#items.get_n_items();
+      this.#items.append(name);
+      this.items_changed(pos, 0, 1);
+  }
+
+  remove(name) {
+      const pos = this.#names.indexOf(name);
+      if (pos < 0)
+          return;
+
+      this.#names.splice(pos, 1);
+
+      this.#settings.block_signal_handler(this.#changedId);
+      this.#settings.set_strv("todoist-search-rules", this.#names);
+      this.#settings.unblock_signal_handler(this.#changedId);
+
+      this.#items.remove(pos);
+      this.items_changed(pos, 1, 0);
+  }
+
+  rename(oldName, newName) {
+      const pos = this.#names.indexOf(oldName);
+      if (pos < 0)
+          return;
+
+      this.#names.splice(pos, 1, newName);
+      this.#items.splice(pos, 1, [newName]);
+
+      this.#settings.block_signal_handler(this.#changedId);
+      this.#settings.set_strv("todoist-search-rules", this.#names);
+      this.#settings.unblock_signal_handler(this.#changedId);
+  }
+
+  vfunc_get_item_type() {
+      return Gtk.StringObject;
+  }
+
+  vfunc_get_n_items() {
+      return this.#items.get_n_items();
+  }
+
+  vfunc_get_item(pos) {
+      return this.#items.get_item(pos);
+  }
+}
+
+
+class NewRuleRow extends Adw.PreferencesRow {
+  static {
+      GObject.registerClass(this);
+  }
+
+  constructor() {
+      super({
+          action_name: 'rules.add',
+          child: new Gtk.Image({
+              icon_name: 'list-add-symbolic',
+              pixel_size: 16,
+              margin_top: 12,
+              margin_bottom: 12,
+              margin_start: 12,
+              margin_end: 12,
+          }),
+      });
+      this.update_property(
+          [Gtk.AccessibleProperty.LABEL], ['Add Rule']);
+  }
+}
+
+
+
+class RuleRow extends Adw.PreferencesRow {
+  static {
+      GObject.registerClass(this);
+  }
+
+  constructor(name) {
+      super({name});
+
+      const box = new Gtk.Box({
+          spacing: 12,
+          margin_top: 6,
+          margin_bottom: 6,
+          margin_start: 6,
+          margin_end: 6,
+      });
+
+      const label = new Gtk.Label({
+          hexpand: true,
+          xalign: 0,
+          max_width_chars: 25,
+          ellipsize: Pango.EllipsizeMode.END,
+      });
+      this.bind_property('name', label, 'label',
+          GObject.BindingFlags.SYNC_CREATE);
+      box.append(label);
+
+      const button = new Gtk.Button({
+          action_name: 'rules.remove',
+          icon_name: 'edit-delete-symbolic',
+          has_frame: false,
+      });
+      box.append(button);
+
+      this.bind_property_full('name',
+          button, 'action-target',
+          GObject.BindingFlags.SYNC_CREATE,
+          (bind, target) => [true, new GLib.Variant('s', target)],
+          null);
+
+      this._entry = new Gtk.Entry({
+          max_width_chars: 25,
+      });
+
+      const controller = new Gtk.ShortcutController();
+      controller.add_shortcut(new Gtk.Shortcut({
+          trigger: Gtk.ShortcutTrigger.parse_string('Escape'),
+          action: Gtk.CallbackAction.new(() => {
+              this._stopEdit();
+              return true;
+          }),
+      }));
+      this._entry.add_controller(controller);
+
+      this._stack = new Gtk.Stack();
+      this._stack.add_named(box, 'display');
+      this._stack.add_named(this._entry, 'edit');
+      this.child = this._stack;
+
+      this._entry.connect('activate', () => {
+          this.activate_action('workspaces.rename',
+              new GLib.Variant('(ss)', [this.name, this._entry.text]));
+          this.name = this._entry.text;
+          this._stopEdit();
+      });
+      this._entry.connect('notify::has-focus', () => {
+          if (this._entry.has_focus)
+              return;
+          this._stopEdit();
+      });
+  }
+
+  edit() {
+      this._entry.text = this.name;
+      this._entry.grab_focus();
+      this._stack.visible_child_name = 'edit';
+  }
+
+  _stopEdit() {
+      this.grab_focus();
+      this._stack.visible_child_name = 'display';
+  }
+}
+
+
+class RulesSettingsWidget extends Adw.PreferencesGroup {
+  static {
+      GObject.registerClass(this);
+
+      this.install_action('rules.add', null,
+          self => self._rules.append());
+      this.install_action('rules.remove', 's',
+          (self, name, param) => self._rules.remove(param.unpack()));
+      this.install_action('rules.rename', '(ss)',
+          (self, name, param) => self._rules.rename(...param.deepUnpack()));
+  }
+
+  constructor() {
+      super({
+          title: 'Rules',
+      });
+
+      this._rules = new RulesList();
+
+      const store = new Gio.ListStore({item_type: Gio.ListModel});
+      const listModel = new Gtk.FlattenListModel({model: store});
+      store.append(this._rules);
+      store.append(new NewItemModel());
+
+      this._list = new Gtk.ListBox({
+          selection_mode: Gtk.SelectionMode.NONE,
+          css_classes: ['boxed-list'],
+      });
+      this._list.connect('row-activated', (l, row) => row.edit());
+      this.add(this._list);
+
+      this._list.bind_model(listModel, item => {
+          return item instanceof NewItem
+              ? new NewRuleRow()
+              : new RuleRow(item.string);
+      });
+  }
+}
+
+
+function init() {}
+
+function fillPreferencesWindow(window) {
+  let gschema = Gio.SettingsSchemaSource.new_from_directory(
+    Me.dir.get_child("schemas").get_path(),
+    Gio.SettingsSchemaSource.get_default(),
+    false
+  );
+
+  settings = new Gio.Settings({
+    settings_schema: gschema.lookup("org.gnome.shell.extensions.mit", true),
+  });
+
+  const page = new Adw.PreferencesPage();
+  const group = new Adw.PreferencesGroup();
+  page.add(group);
+
+  // const title = new Gtk.Label({
+  //     label: `<b>${Me.metadata.name} Preferences</b>`,
+  //     halign: Gtk.Align.START,
+  //     use_markup: true,
+  //     visible: true,
+  // });
+  // group.add(title);
+
+  const todoistTokenLabel = new Gtk.Label({
+    label: "Todoist API Token:",
+    halign: Gtk.Align.START,
+    visible: true,
+  });
+  group.add(todoistTokenLabel);
+
+  const todoistTokenEntry = new Gtk.Entry({
+    visible: true,
+    text: settings.get_string("todoist-api-token"),
+  });
+
+  group.add(todoistTokenEntry);
+  settings.bind(
+    "todoist-api-token",
+    todoistTokenEntry,
+    "text",
+    Gio.SettingsBindFlags.DEFAULT
+  );
+
+  const todoistSearchLabel = new Gtk.Label({
+    label: "Todoist API Search:",
+    halign: Gtk.Align.START,
+    visible: true,
+  });
+  group.add(todoistSearchLabel);
+
+  const todoistSearchEntry = new Gtk.Entry({
+    visible: true,
+    text: settings.get_string("todoist-search"),
+  });
+
+  group.add(todoistSearchEntry);
+  settings.bind(
+    "todoist-search",
+    todoistSearchEntry,
+    "text",
+    Gio.SettingsBindFlags.DEFAULT
+  );
+
+  page.add(new RulesSettingsWidget());
+
+
+
+
+  window.add(page);
 }
